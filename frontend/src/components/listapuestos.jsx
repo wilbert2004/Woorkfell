578c2e49913f21457d96027d8dde831b6b82e45a
Crhistopher Gutiérrez <66912225+CrhistopherG@users.noreply.github.com>
2025-01-29 18:13:39 -0600

    Update listapuestos.jsx

--- frontend/src/components/listapuestos.jsx
+++ frontend/src/components/listapuestos.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { CiUser } from "react-icons/ci";
 
 const Listapuestos = () => {
   return (
     <div>
       <h2>componente de lista de puesto</h2>
+      <h1>kj sbsb</h1>
+      <CiUser />
+      <br></br>
+      <button className="rounded-pill">Haz click aqui</button>
     </div>
+    
   );
 };
 
