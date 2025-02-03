2995b4d950ad672fbf3a406509d95b93713d35d5
Crhistopher Gutiérrez <crhistopherisai@gmail.com>
2025-02-03 15:11:19 -0600

    ad

--- frontend/src/components/listapuestos.jsx
+++ frontend/src/components/listapuestos.jsx
@@ -4,9 +4,7 @@ import Home from "./home";
 const Listapuestos = () => {
   return (
     <Home>
-      <div>
-        <h2>componente de lista de puesto</h2>
-      </div>
+      
     </Home>
   );
 };
