fbec8f70836115e84e0911e767e727295f85feb2
Crhistopher Gutierrez <crhistopherisai@gmail.com>
2025-02-05 17:05:34 -0600

    Actualizado componente configuraciones

--- frontend/src/components/configuracion.jsx
+++ frontend/src/components/configuracion.jsx
@@ -1,16 +1,62 @@
 import React from "react";
 import Home from "./home";
-
+import { Link } from "react-router-dom";
+import { RiUserSettingsLine } from "react-icons/ri";
 const Configuracion = () => {
   return (
     <Home>
-      <div>
-        <h1>Hola wilbert chan </h1>
-        <h1>Hola wilbert chan </h1>
-        <h1>Hola wilbert chan </h1>
-        <h1>Hola wilbert chan </h1>
-        <h1>Hola wilbert chan </h1>
-        <h1>Hola wilbert chan </h1>
+      <div className="container-fluid">
+        <div className="navbar">
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <Link to="/configuracion" className="link-secondary">
+                    <RiUserSettingsLine style={{ marginRight: "3px" }} />
+                  </Link>
+                </li>
+                <li
+                  className="breadcrumb-item active"
+                  aria-current="page"
+                  style={{ marginTop: "2px" }}
+                >
+                  <Link to="/configuracion" className="link-secondary">
+                    Configuracion
+                  </Link>
+                </li>
+              </ol>
+            </nav>
+          </div>
+
+          <div
+            className="container border text-center w-100 d-flex flex-column justify-content-evenly align-items-center"
+          >
+            <div>
+              <p className="h2 text-muted">
+                Selecciona el periodo
+              </p>
+            </div>
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Rango de interpretacion de grupos o variables
+              </button>
+              <ul className="dropdown-menu">
+                {["Soporte IT", "RRHH", "Contabilidad", "Mantenimiento", "Gerencia"].map((dept) => (
+                  <li key={dept}>
+                    <button className="dropdown-item" type="button">
+                      {dept}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+
+
+
+
+          </div>
+
+
       </div>
     </Home>
   );
