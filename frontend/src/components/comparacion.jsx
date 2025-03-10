66da05f92c87741e19c396f443b74b964784d9c4
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-03-10 13:02:04 -0600

    coparaciones en proceso
    desarrollando el modulo de comparaciones , aun esta en proceso pero lo subo mañana sigo desarrollandolo

--- frontend/src/components/comparacion.jsx
+++ frontend/src/components/comparacion.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import Home from "./home";
+import { BsReceipt } from "react-icons/bs";
+
+import { Link } from "react-router-dom";
+
 const Comparacion = () => {
   return (
     <Home>
-      <div>
-        <h2>soy el componente de comparacion </h2>
+      <div className="container">
+        <div className="navbar">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link to="/Comparacion" className="link-secondary">
+                  <BsReceipt style={{ marginRight: "10px" }} />
+                </Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                <Link to="/Comparacion" className="link-secondary">
+                  Resultados
+                </Link>
+              </li>
+            </ol>
+          </nav>
+        </div>
+        <div className="row justify-content-center col-md-10  text-muted">
+          <p>
+            Indicaciones: En este modulo podras comparar los resultados entre
+            dos periodos de manera elegidos , podras mostrar las variables
+            <br />
+            -grupos o las preguntas o ambas cosas en elemento de visualizacion y
+            filtrar
+          </p>
+        </div>
       </div>
     </Home>
   );
