7debcdbc5ad144a430ecf1c8472efdc649152e18
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-03-12 10:02:29 -0600

    asignando mis avances de uevo que se cancelo
    agregando mis avences de nuevo que se cancelo , esta en proceso de desarrollo

--- frontend/src/components/comparacion.jsx
+++ frontend/src/components/comparacion.jsx
@@ -1,10 +1,141 @@
 import React from "react";
 import Home from "./home";
+import Checkboxutili from "./checkboxutili";
+import Filtroresulta from "./filtroresulta";
+import { Link, useNavigate } from "react-router-dom";
+import { BsReceipt } from "react-icons/bs";
+
 const Comparacion = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/infocompar");
+  };
+
   return (
     <Home>
-      <div>
-        <h2>soy el componente de comparacion </h2>
+      <div className="container">
+        <div className="navbar">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link to="/Resultados" className="link-secondary">
+                  <BsReceipt style={{ marginRight: "10px" }} />
+                </Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                <Link to="/Comparacion" className="link-secondary">
+                  Comparacion
+                </Link>
+              </li>
+            </ol>
+          </nav>
+        </div>
+        <div>
+          <p className="opacity-50 p-2">
+            Indicacionces: En este modulo podras comparar los resultados entre
+            dos periodos de manera elegidos , podras mostrar las variables
+            -grupos o las preguntas o ambas cosas en elemento de visualizacion y
+            filtarr{" "}
+          </p>
+
+          <div className="col-xs-12 border rounded text-center">
+            <p className="h4 m-1">Selecciona tu periodo a comparar </p>
+            <div className="dropdown ">
+              <button
+                type="button"
+                className="btn  dropdown-toggle m-1  w-75"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Selecciona tu primer periodo
+              </button>
+              <ul className="dropdown-menu col-xs-12 text-center w-75">
+                {["Periodo 1 Enero - Febrero", "Periodo 2 Marzo  - Mayo "].map(
+                  (item) => (
+                    <li key={item}>
+                      <button className="dropdown-item" type="button">
+                        {item}
+                      </button>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          </div>
+          <br />
+          {/* este div sirve para el segundo comparacion de nuestro modulo  */}
+          <div className="col-xs-12 border rounded text-center">
+            <p className="h4 m-1">Selecciona tu periodo a comparar </p>
+            <div className="dropdown ">
+              <button
+                type="button"
+                className="btn  dropdown-toggle m-1  w-75"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Selecciona tu segundo periodo a comparar
+              </button>
+              <ul className="dropdown-menu col-xs-12 text-center w-75">
+                {["Periodo 1 Enero - Febrero", "Periodo 2 Marzo  - Mayo "].map(
+                  (item) => (
+                    <li key={item}>
+                      <button className="dropdown-item" type="button">
+                        {item}
+                      </button>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          </div>
+          <br />
+          {/* tercer div para visualizar lo que queremos mostrar */}
+          <div className="col-xs12 border rounded text-center">
+            <p className="h4 m-1">Elemento de visualizacion </p>
+            <div className="dropdown">
+              <button
+                className="btn  dropdown-toggle m-1  w-100"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Comparar todo
+              </button>
+              <ul className="dropdown-menu col-xs-12 text-center w-75">
+                {["Resultados por grupo", "Resultados por pregunta"].map(
+                  (item) => (
+                    <li key={item}>
+                      <button className="dropdown-item" type="button">
+                        {item}
+                      </button>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          </div>
+          {/* componente reutilizables de filtros  */}
+          <div>
+            {/* contenedor de filtro de datos */}
+            <Filtroresulta />
+          </div>
+          {/* menu desplazamiento de mostrar desglose  con dimensiones en grafica  */}
+          <div>
+            {/* metemos el componente del checbox utilizable  */}
+            <Checkboxutili />
+          </div>
+          {/* boton para realizar la comparacion */}
+          <div className="col-xs-12 text-center mt-5">
+            <button
+              type="button"
+              className="btn btn-primary w-25"
+              onClick={handleNavigate}
+            >
+              Realizar Comparación
+            </button>
+          </div>
+        </div>
       </div>
     </Home>
   );
---
+++ frontend/src/components/tableinterpretacion.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import Cuadrado from "./cuadrado";
+
+const Tableinterpretacion = () => {
+  return (
+    <div className="container">
+      <div className="border text-center m-3">
+        <h3 className="bg-dark text-white opacity-50">Interpretacion</h3>
+        <p className="opacity-50">
+          los grados de satisfaccion se divide en tres partes (favorables
+          ,indeciso y desaforable). los valores que les corresponde son
+          configurados desde el modulo de configuraciones . Los valores
+          permitidos son los correpondientes a las escalas desde el modulo{" "}
+        </p>
+
+        <div
+          className="row "
+          style={{
+            marginLeft: "5px",
+          }}
+        >
+          <div className="col-sm ">
+            <span>Rangos por grupos o variables </span>
+          </div>
+          <div className="col-sm">Rangos por preguntas </div>
+        </div>
+        {/* intervalo color verder */}
+        <div
+          className="row article text-center "
+          style={{
+            marginLeft: "50px",
+          }}
+        >
+          <div className="col-sm ">
+            <div className="d-flex flex-row">
+              <div className="p-2">
+                <Cuadrado color="green" width="30px" height="30px" />
+              </div>
+              <div className="p-2">Opinion favorables (4 a 5)</div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div className="d-flex flex-row">
+              <div className="p-2">
+                <Cuadrado color="green" width="30px" height="30px" />{" "}
+                {/* Cuadrado verde */}
+              </div>
+              <div className="p-2">Opinion favorables (4 a 5)</div>
+            </div>
+          </div>
+        </div>
+        {/* intervalo de amarillo */}
+        <div
+          className="row article text-center "
+          style={{
+            marginLeft: "50px",
+          }}
+        >
+          <div className="col-sm">
+            <div className="d-flex flex-row">
+              <div className="p-2">
+                <Cuadrado color="yellow" width="30px" height="30px" />{" "}
+                {/* Cuadrado amarillo */}
+              </div>
+              <div className="p-2">Opinion indecisa (2 a 3.9)</div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div className="d-flex flex-row">
+              <div className="p-2">
+                <Cuadrado color="yellow" width="30px" height="30px" />{" "}
+                {/* Cuadrado amarillo */}
+              </div>
+              <div className="p-2">Opinion indecisa (2 a 3.9)</div>
+            </div>
+          </div>
+        </div>
+        {/* intervalo de rojo */}
+        <div
+          className="row article text-center  "
+          style={{
+            marginLeft: "50px",
+          }}
+        >
+          <div className="col-sm">
+            <div className="d-flex flex-row">
+              <div className="p-2">
+                <Cuadrado color="red" width="30px" height="30px" />{" "}
+                {/* Cuadrado rosa */}
+              </div>
+              <div className="p-2">Opinion Desafavorables (1 a 1.9)</div>
+            </div>
+          </div>
+
+          <div className="col-sm">
+            <div className="d-flex flex-row">
+              <div className="p-2">
+                <Cuadrado color="red" width="30px" height="30px" />{" "}
+                {/* Cuadrado rosa */}
+              </div>
+              <div className="p-2">Opinion Desafavorables (1 a 1.9)</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tableinterpretacion;
